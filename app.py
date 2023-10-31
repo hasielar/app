2a80a44edb091625713318ccdd1952be15adb84e
hasielar <144086255+hasielar@users.noreply.github.com>
2023-10-31 23:31:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-# Title
-st.title('Weight Goal Setter App')
 
-#Sub Header
-st.subheader("Hi.I'm Huslen and this is my app :wave:")
-st.write("soooooo")
+# Title
+st.title('Weight Tracking App')
 
 # Sidebar
 st.sidebar.header('User Input')
@@ -42,33 +38,20 @@ else:
     st.warning('Keep working towards your weight goal.')
 
 # App description
-import streamlit as st
+st.text('This app allows you to set a weight goal and track your progress over time.')
 
-# Introduction
-st.title("Welcome to the Weight Tracking App")
-st.text("This is a simple tool to help you monitor your weight over time and work towards your weight goal. "
-        "Whether you're aiming to lose, gain, or maintain your weight, this app can assist you in visualizing your progress.")
+# Instructions and how to use the app
+st.header('Instructions:')
+st.markdown('1. Use the sidebar to enter your current weight and set your target weight goal.')
+st.markdown('2. In the "Weight Tracking" section, input your weight data over time, separated by commas.')
+st.markdown('3. Observe the line chart that displays your weight history and see your progress.')
+st.markdown('4. Check the "Weight Goal Progress" section to see if you have achieved your goal.')
 
-# How to Use
-st.header("How to Use:")
-st.text("1. **User Input**:")
-st.text("   - On the left-hand side, you'll find a sidebar labeled 'User Input.' Here, you can set your weight-related parameters:")
-st.text("     - **Current Weight**: Input your current weight (in kilograms) by typing it into the 'Enter your weight' field.")
-st.text("     - **Target Weight**: Set your desired target weight by entering the value in the 'Set your target weight' field.")
+# Tips
+st.markdown('Tips:')
+st.markdown('- Consistency is key to achieving your weight goal.')
+st.markdown('- Monitor your progress and stay motivated.')
 
-st.text("2. **Weight Data Entry**:")
-st.text("   - Below the 'User Input' section, you'll see a text area labeled 'Weight Tracking.' Here, you can input your weight data over time.")
-st.text("   - Enter your weight readings, separated by commas, in chronological order. For example, if you have recorded your weight for three days, you can input it as '70, 69, 68,' with the most recent weight as the last entry.")
-
-st.text("3. **Visualization**:")
-st.text("   - Once you've entered your weight data, the app will generate a line chart that displays your weight changes over time. The x-axis represents the number of data points (1, 2, 3, ...) and the y-axis represents the corresponding weight values.")
-
-st.text("4. **Goal Progress**:")
-st.text("   - The app calculates and displays your progress toward your target weight goal.")
-st.text("   - If your current weight is less than your target weight, you'll see a success message indicating that you've achieved your goal.")
-st.text("   - If your current weight is higher than your target weight, you'll receive a warning to keep working toward your goal.")
-
-st.text("Use this app to keep track of your weight, stay motivated, and achieve your weight goals. "
-        "Remember that consistency and dedication are key to a healthier you!")
+# Enjoy using the app!
 
 
